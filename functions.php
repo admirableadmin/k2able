02d9d13bf1f7def2056c7cf9c7e84c2f13c23ba5
Andreas Peichert <andpei@users.noreply.github.com>
2021-12-11 11:04:54 +0100

    added countdown

--- functions.php
+++ functions.php
@@ -117,4 +117,27 @@ function overwrite_header_image_when_away_tag_is_shown() {
 	<?php endif;
 }
 
+// countdown
+// https://www.w3schools.com/howto/howto_js_countdown.asp
+add_action('wp_head', 'add_countdown');
+function add_countdown() {
+?>
+<script>
+var countDownDate = new Date("March 1, 2022 9:00:00").getTime();
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+ if (distance < 0) {
+    clearInterval(x);
+  } else {
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  }
+}, 1000);
+</script>
+<?php
+}
 ?>
